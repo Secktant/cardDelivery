cf3d9d47c89b135a57786920401344d0fea38701
Nikita Santalov <na.santalov@yandex.ru>
2022-09-03 23:04:31 +0400

    init commit

---
+++ src/main/java/Data/DataGenerator.java
@@ -0,0 +1,41 @@
+package Data;
+
+import Data.UserInfo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import com.github.javafaker.Faker;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static Faker faker = new Faker(new Locale("ru"));//рус база
+
+    public static String generateDate(int addDays) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+        }
+    }
+}
---
+++ src/test/java/СardTests.java
@@ -0,0 +1,46 @@
+import Data.DataGenerator;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class СardTests {
+
+    @BeforeEach
+    public void openPage() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void ChangeDate() {
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        val daysToAddForFirstMeeting = 4;
+        val firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        val daysToAddForSecondMeeting = 7;
+        val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $x("//input[@placeholder=\"Город\"]").val(validUser.getCity());
+        $x("//input[@type=\"tel\"]").doubleClick().sendKeys(Keys.DELETE);
+        $x("//input[@placeholder=\"Дата встречи\"]").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $x("//*[contains(text(),'Успешно!')]").should(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification] .notification__content").should(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        //$x("//input[@type=\"tel\"]").doubleClick().sendKeys("DELETE");
+        $x("//input[@type=\"tel\"]").doubleClick().sendKeys(Keys.DELETE);
+        $x("//input[@placeholder=\"Дата встречи\"]").val(secondMeetingDate);
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $("[data-test-id=replan-notification]").should(visible, Duration.ofSeconds(15));
+        $x("//*[contains(text(),'Перепланировать')]").click();
+        $("[data-test-id=success-notification] .notification__content").should(visible, Duration.ofSeconds(15)).should(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
